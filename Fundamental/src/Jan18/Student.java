480c9db5d645be39cc513068ac7c95add63767bf
bishwajavaproject <my email>
2023-01-18 23:41:11 -0500

    Student.java completed

--- Fundamental/src/Jan18/Student.java
+++ Fundamental/src/Jan18/Student.java
@@ -1,5 +1,22 @@
 package Jan18;
 
 public class Student {
+	
+	public static void main(String[] args) {
+		//printing student detail
+		int id;
+		String fname, lname, address;
+		id = 101;
+		fname = "Tool";
+		lname = "Mack";
+		address = "New York";
+		
+		System.out.println("---DETAIL OF STUDENT---");
+		System.out.println("_____________________");
+		System.out.println("ID: " + id);
+		System.out.println("First Name: " + fname);
+		System.out.println("Last Name: " + lname);
+		System.out.println("Address: " + address);
+	}
 
 }
