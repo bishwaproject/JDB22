2a136e45f67a736fce93cabe1d4a42c63ad7f975
bishwajavaproject <you@example.com>
2023-01-21 12:35:18 -0500

    SwapThreeWithoutTempMethod completed

--- Fundamental/src/Jan21/SwapThreeWithoutTempMethod.java
+++ Fundamental/src/Jan21/SwapThreeWithoutTempMethod.java
@@ -1,5 +1,34 @@
 package Jan21;
 
+import Jan18.SwapThreeWithoutTemp;
+
 public class SwapThreeWithoutTempMethod {
+	void swappingTask(int n1, int n2, int n3) {
+		n1 = n1 + n2 + n3; 
+		n3 = n1 - n2 - n3;
+		n2 = n1 - n2 - n3; 
+		n1 = n1 - n2 - n3;
+		System.out.println("After Swapping: ");
+		System.out.println("N1 = " + n1);
+		System.out.println("N2 = " + n2);
+		System.out.println("N3 = " + n3);
+	}
+
+	public static void main(String[] args) {
+		int n1, n2, n3;
+		n1 = 100;
+		n2 = 200;
+		n3 = 300;
+		System.out.println("Before Swapping: ");
+		System.out.println("N1 = " + n1);
+		System.out.println("N2 = " + n2);
+		System.out.println("N3 = " + n3);
+		
+		//call swappingTask()
+		SwapThreeWithoutTempMethod swap = new SwapThreeWithoutTempMethod();
+		swap.swappingTask(n1, n2, n3);
+	
+		
+	}
 
 }
